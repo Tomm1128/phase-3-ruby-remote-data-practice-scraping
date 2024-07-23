060ded33f2d52c06455c79bb1f705064c6dcdec5
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-23 13:47:22 -0400

    Scrap data from flatiron website

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css("section.column-highlights:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)")
+
+courses.each do |course|
+  puts course.text.strip
+end
